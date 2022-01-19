2db61d4f4fe8853be4481849a230951fefff67df
rob.brz <robert.brzozowski@ftsgps.com>
2022-01-19 15:20:10 +0100

    Add project files.

---
+++ DockerTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DockerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Task.Delay(2000).Wait();
+                    Console.WriteLine("cos");
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                } 
+            }
+        }
+    }
+}
